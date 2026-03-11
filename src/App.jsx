a7845264a26c27ff50b845a2da7d7e308146ac95
Supaporn Kimpagaew <67160413@go.buu.ac.th>
2026-03-11 13:48:40 +0700

    feat: add counter and add input message

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+
+  // useEffect ที่ทำงานเพียงครั้งแรกเท่านั้น
+  useEffect(() => {
+    console.log("Component mounted");
+  }, []);
+
+  // useEffect ที่ทำงนทุกครั้งที่ count เปลี่ยน
+  useEffect(() => {
+    console.log(`Count เปลี่ยนเป็น: ${count}`);
+  }, [count]);
+
+  // useEffect ทำงานเมื่อ message เปลี่ยน
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log(`💬 Message: ${message}`);
+    }, 2000); //ทำงานทุก 2 วินาที
+
+    //เคลียร์ฟังก์ชัน
+    return () => clearTimeout(timer);
+  }, [message]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style={{ padding: "20px" }}>
+      <h1>🎯 useEffect Examples</h1>
 
-export default App
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+
+      <div style={{ marginTop: "20px" }}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="พิมพ์ข้อความ"
+        />
+        <p>Message: {message}</p>
+      </div>
+
+      <p style={{ color: "#999", fontSize: "12px" }}>
+        ดูที่ Console (F12) เพื่อเห็น logs
+      </p>
+    </div>
+  );
+}
