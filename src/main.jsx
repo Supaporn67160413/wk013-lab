be7004285aef30074257e065a3cf2bfb14d53cc4
Supaporn Kimpagaew <67160413@go.buu.ac.th>
2026-03-11 16:33:26 +0700

    feat: Add page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx"; // รับเข้า แล้วนำมาเป็นชื่อแท็กเลย
+import UserList from "./UserList.jsx";
 
-createRoot(document.getElementById('root')).render(
+//หา Element ที่ชื่อ root แล้วก็จะ render
+//reder คือ การเปลี่ยนแปลงของ Component
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    {/* แทนที่ด้วย Export มาทั้งหมด*/}
     <App />
+    <UserList />
   </StrictMode>,
-)
+);
